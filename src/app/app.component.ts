aecf36cf6dcfbe93b8222f09df06f0907bb65407
dshimmyroy <dshimmyroy@gmail.com>
2021-12-22 16:25:18 +0530

    Added some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchDataService } from './fetch-data.service';
+import { fetchedDataType } from './posts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-API-call';
+export class AppComponent implements OnInit{
+  
+  public fetchedData : fetchedDataType[] = [];
+
+  constructor(private _fetchDataService : FetchDataService){}
+
+  ngOnInit(){
+    this._fetchDataService.getData()
+        .subscribe(data => this.fetchedData = data );
+  }
 }
